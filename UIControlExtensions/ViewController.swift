855a14af1195b9bb648a36d9cd1999f3f5b89bd9
Umer Khan <umar_s66@outlook.com>
2020-03-23 06:33:44 +0500

    Initial Commit

---
+++ UIControlExtensions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIControlExtensions
+//
+//  Created by Umer Khan on 23/03/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
